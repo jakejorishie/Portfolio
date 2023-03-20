dd8c427db96cf066fa7f380e157789fcd57c0fd8
Jacob Joseph Jorishie <jake.jorishie@gmail.com>
2023-03-20 16:57:09 -0500

    second commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Navbar</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
